9564b058f7cd7116bb4a5602438fff6bb0fde7d4
sonangrai <shonahangrae@gmail.com>
2021-02-01 11:21:18 +0545

    removed layoutjs and intergrated it in routes of admin

--- frontend/src/components/admin/routes.js
+++ frontend/src/components/admin/routes.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Layout from "./layout/Layout";
 import login from "./auth/Login";
 import Notfound from "./Notfound";
 import Forgot from "./auth/Forgot";
 import Newpassword from "./auth/Newpassword";
 
-const routes = () => {
+import Footer from "./layout/Footer";
+import Sidebar from "./layout/Sidebar";
+import Header from "./layout/Header";
+import Home from "./Home";
+
+import { connect } from "react-redux";
+import Profile from "./auth/Profile";
+
+const routes = ({ isAuthenticated }) => {
   return (
     <Fragment>
       <Router>
@@ -18,7 +25,25 @@ const routes = () => {
             path="/admin/recover/:uid/:tid"
             component={Newpassword}
           />
-          <Route exact path="/admin/" component={Layout} />
+          {isAuthenticated ? (
+            <div className="dash__row">
+              <div className="sidebar__part">
+                <Sidebar />
+              </div>
+              <div className="maincontent__part">
+                <Header />
+                <div className="contents__box">
+                  <Switch>
+                    <Route exact path="/admin" component={Home} />
+                    <Route exact path="/admin/profile" component={Profile} />
+                  </Switch>
+                </div>
+                <Footer />
+              </div>
+            </div>
+          ) : (
+            <Route component={login} />
+          )}
           <Route component={Notfound} />
         </Switch>
       </Router>
@@ -26,4 +51,8 @@ const routes = () => {
   );
 };
 
-export default routes;
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(routes);

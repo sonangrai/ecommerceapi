41e250e731bfb2870aea082582ed5bee20447e13
sonangrai <shonahangrae@gmail.com>
2021-02-01 11:03:49 +0545

    added the screen loader

--- frontend/src/components/admin/home.js
+++ frontend/src/components/admin/home.js
@@ -1,22 +1,12 @@
 import React, { Fragment } from "react";
+import Loader from "./layout/Loader";
 import Title from "./layout/Title";
 
 const Home = () => {
   return (
     <Fragment>
       <Title title="Dashboard - Pasal" />
-      <img
-        src="https://cdn.pixabay.com/photo/2021/01/21/14/10/egret-5937499__340.jpg"
-        alt=""
-      />
-      <img
-        src="https://cdn.pixabay.com/photo/2021/01/21/14/10/egret-5937499__340.jpg"
-        alt=""
-      />
-      <img
-        src="https://cdn.pixabay.com/photo/2021/01/21/14/10/egret-5937499__340.jpg"
-        alt=""
-      />
+      <Loader />
     </Fragment>
   );
 };
---
+++ frontend/src/components/admin/layout/Loader.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+
+const Loader = () => {
+  return (
+    <Fragment>
+      <div className="load__cont">
+        <div className="loader">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Loader;

57a8259f156c756944794a0b2e73ba0f8556b7dd
sonangrai <shonahangrae@gmail.com>
2021-02-07 11:47:18 +0545

    styled the table

--- frontend/src/components/admin/products/Viewproducts.js
+++ frontend/src/components/admin/products/Viewproducts.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { allproduct } from "../../../actions/product";
 import Loader from "../layout/Loader";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Viewproducts = ({ allproduct, product }) => {
   useEffect(() => {
@@ -12,19 +13,18 @@ const Viewproducts = ({ allproduct, product }) => {
   return (
     <Fragment>
       {product ? (
-        <div className="">
-          <div className="">
-            <table>
+        <div className="view__cnt">
+          <div className="tbl__cont">
+            <table className="tbl">
               <thead>
                 <tr>
                   <th>S. No</th>
                   <th>Name</th>
-                  <th>Description</th>
+                  <th>Color</th>
                   <th>Category</th>
                   <th>Sub Category</th>
                   <th>Rate</th>
-                  <th>Amount</th>
-                  <th>Added</th>
+                  <th>Inventory Amount</th>
                   <th>Actions</th>
                 </tr>
               </thead>
@@ -33,13 +33,19 @@ const Viewproducts = ({ allproduct, product }) => {
                   <tr key={i}>
                     <td>{i + 1}</td>
                     <td>{prod.name}</td>
-                    <td>{prod.description}</td>
+                    <td>{prod.color}</td>
                     <td>{prod.category}</td>
                     <td>{prod.subcategory}</td>
                     <td>{prod.rate}</td>
-                    <td>{prod.amount}</td>
-                    <td>{prod.createdAt}</td>
-                    <td>{prod.createdAt}</td>
+                    <td>{prod.invamount}</td>
+                    <td>
+                      <Link
+                        className="btn primary-btn"
+                        to={`/product/${prod._id}`}
+                      >
+                        View
+                      </Link>
+                    </td>
                   </tr>
                 ))}
               </tbody>

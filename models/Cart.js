5f971ac941dba37da1e3c1129e2bb624ec6052c6
sonangrai <shonahangrae@gmail.com>
2021-02-17 11:00:57 +0545

    updated the cart model

--- models/Cart.js
+++ models/Cart.js
@@ -1,8 +1,13 @@
 const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema({
+  userid: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
   productid: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
   },
   quantity: {
     type: Number,

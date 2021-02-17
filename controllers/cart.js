9e4779a5374398dae6893ecb3cb7dc2edef8163d
sonangrai <shonahangrae@gmail.com>
2021-02-17 11:39:23 +0545

    add cart item api done

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,3 +1,21 @@
+const Cart = require("../models/Cart");
+
+//Adding item to Cart
 exports.addcart = async (req, res) => {
-  res.send("HOLa");
+  const { quantity } = req.body;
+  const userid = req.params.uid;
+  const productid = req.params.pid;
+
+  const cart = new Cart({
+    userid,
+    productid,
+    quantity,
+  });
+
+  try {
+    await cart.save();
+    res.json(cart);
+  } catch (error) {
+    res.json(error);
+  }
 };
--- routes/cart.js
+++ routes/cart.js
@@ -3,6 +3,6 @@ const { addcart } = require("../controllers/cart");
 const router = express.Router();
 
 //Adding item to cart
-router.post("/cart/", addcart);
+router.post("/cart/:pid/:uid", addcart);
 
 module.exports = router;

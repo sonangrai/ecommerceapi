2da6bb2ca55b693450ff3c14ff573f3167e94a44
sonangrai <shonahangrae@gmail.com>
2021-02-19 11:28:39 +0545

    controller and routes for oder

---
+++ controllers/order.js
@@ -0,0 +1,1 @@
+exports.placeorder = async (req, res) => {};
---
+++ routes/order.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { placeorder } = require("../controllers/order");
+const router = express.Router();
+
+//Placing the order
+router.post("/order/", placeorder);
